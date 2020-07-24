af5e59b09321e56ef4a46fe29e187bd295b020f6
Tigran <tigr24an7@users.noreply.github.com>
2020-07-24 20:55:02 +0300

    my first commit on cookie-clier

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,12 @@
+const image = document.getElementById("cookie");
+const clicker__counter = document.getElementById("clicker__counter")
+function changeSizes(){
+    if (image.width==200) {
+        image.width = 150;
+    }
+    else if (image.width==150){
+        image.width = 200;
+    }
+    clicker__counter.textContent = Number(clicker__counter.textContent)+1
+};
+image.onclick=changeSizes;
