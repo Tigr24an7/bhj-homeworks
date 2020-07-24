3a6b9e6af8940f782b7678a300c086937c374f03
Tigran <tigr24an7@users.noreply.github.com>
2020-07-24 20:43:34 +0300

    my first commit on mole-game

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,27 @@
+let gethole = index => document.getElementById(`hole${index}`);
+const dead = document.getElementById('dead');
+const lost = document.getElementById('lost');
+
+
+for (i = 1; i <= 9; i++) {
+    gethole(i).onclick = function() {
+        if (this.className.includes('hole_has-mole')) {
+            dead.textContent = Number(dead.textContent) + 1
+        }
+        else {
+            lost.textContent = Number(lost.textContent) + 1
+        }
+
+        if (dead.textContent == 10){
+            alert("Вы выиграли")
+            dead.textContent = 0
+            lost.textContent = 0
+        }
+        else if (Number(lost.textContent) == 5){
+            alert("Вы проиграли")
+            dead.textContent = 0
+            lost.textContent = 0
+        }
+
+    }
+}
