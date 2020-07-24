c9811b46404e640f45137d5c394c299c60f1f38b
Tigran <tigr24an7@users.noreply.github.com>
2020-07-24 20:53:49 +0300

    my first commit on countdown

---
+++ js-features/countdown/task.js
@@ -0,0 +1,9 @@
+const timer=document.getElementById("timer");
+function timerFunction(){
+timer.textContent = Number(timer.textContent)-1;
+    if (timer.textContent==0){
+        clearInterval(interval);
+        alert("Вы победили в конкурсе!")
+    }
+}
+let interval = setInterval(timerFunction,1000)
